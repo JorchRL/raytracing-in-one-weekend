d0e22dd7d9ce621dc6c993ac5b0b4cc7d781ea9b
Jorge <jorch.rl@gmail.com>
2022-08-29 18:53:47 -0500

    Gamma correction

--- common/color.h
+++ common/color.h
@@ -12,9 +12,9 @@ void write_color(std::ostream &out, color pixel_color, int samples) {
   auto b = pixel_color.z();
 
   auto scale = 1.0 / samples;
-  r *= scale;
-  g *= scale;
-  b *= scale;
+  r = sqrt(scale * r);
+  g = sqrt(scale * g);
+  b = sqrt(scale * b);
 
   out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
     << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
