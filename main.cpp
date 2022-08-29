f4736cda472f4b03253b6f08ecfab7a148ab0dc4
Jorge <jorch.rl@gmail.com>
2022-08-29 18:54:06 -0500

    change bounce and sample settings (slow)

--- main.cpp
+++ main.cpp
@@ -32,10 +32,10 @@ color ray_color(const ray& r, const hittable& world, int bounces) {
 int main() {
 
   const auto aspect_ratio = 16.0 / 9.0;
-  const int image_width = 800;
+  const int image_width = 600;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-  const int samples_per_pixel = 2;
+  const int samples_per_pixel = 100;
   const int bounces = 50;
 
   // world
